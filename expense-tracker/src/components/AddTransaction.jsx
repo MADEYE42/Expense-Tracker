1ae70f393bdcc1124a6743e871f95ce74129d47b
Gouresh  Madye <117588160+MADEYE42@users.noreply.github.com>
2024-06-15 19:26:09 +0530

    Expense tracker frontend done

---
+++ expense-tracker/src/components/AddTransaction.jsx
@@ -0,0 +1,36 @@
+import { useContext } from 'react';
+import React,{useState} from 'react'
+import { GlobalContext } from '../context/GlobalState';
+const AddTransaction = () => {
+    const [text,setText] = useState('');
+    const [amount,setAmount] = useState(0);
+    const {addTransaction} = useContext(GlobalContext)
+    const onSubmit =e=>{
+        e.preventDefault();
+        const newTransaction = {
+            id:Math.floor(Math.random()*10000000),
+            text,
+            amount:+amount  
+        }
+        addTransaction(newTransaction); 
+    }
+
+  return (
+    <div>
+    <h3>Add New Transactions</h3>
+    <form id='form' onSubmit={onSubmit}>
+    <div className="form-control">
+        <label htmlFor="text">Text</label>
+        <input type='text' id='text' value={text} onChange={(e)=>setText(e.target.value)} placeholder='Enter text...' />
+    </div>
+    <div className="form-control">
+        <label htmlFor="amount">Amount<br/>(negative : expense, positive : income)</label>
+        <input type='number' value={amount} onChange={(e)=>setAmount(e.target.value)} id='amount'placeholder='Enter text...' />
+    </div>
+    <button  className="btn">Add Transaction</button>
+    </form>
+    </div>
+  )
+}
+
+export default AddTransaction
