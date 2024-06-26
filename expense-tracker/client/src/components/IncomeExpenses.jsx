63e600e6f337726c29ed0aecc78cda324fcb73d2
Gouresh  Madye <117588160+MADEYE42@users.noreply.github.com>
2024-06-26 13:30:10 +0530

    Done with connecting backend and frontend

--- expense-tracker/client/src/components/IncomeExpenses.jsx
+++ expense-tracker/client/src/components/IncomeExpenses.jsx
@@ -1,5 +1,6 @@
 import React,{useContext} from 'react'
 import { GlobalContext } from '../context/GlobalState';
+import {numberWithCommas} from '../utils/formats';
 
 const IncomeExpenses = () => {
   const {transactions} =useContext(GlobalContext);
@@ -11,11 +12,11 @@ const IncomeExpenses = () => {
     <div className="inc-exp-container">
         <div>
             <h4>Income</h4>
-            <p className="money plus" id="money-plus">₹{income}</p>
+            <p className="money plus" id="money-plus">₹{numberWithCommas(income)}</p>
         </div>
         <div>
             <h4>Expense</h4>
-            <p className="money minus" id='money-minus'>₹{expense}</p>
+            <p className="money minus" id='money-minus'>₹{numberWithCommas(expense)}</p>
         </div>
     </div>
   )
--- expense-tracker/client/src/context/GlobalState.jsx
+++ expense-tracker/client/src/context/GlobalState.jsx
@@ -1,28 +1,70 @@
 import React,{createContext,useReducer} from 'react';
-import AppReducer from './AppReducer'
+import AppReducer from './AppReducer';
+import axios from 'axios'
+
 const initialState = {
-    transactions : []
+    transactions : [],
+    error:null,
+    loading:true
 }
 export const GlobalContext = createContext(initialState)
 export const GlobalProvider = ({children}) =>{
     const [state,dispatch] = useReducer(AppReducer,initialState);
 
-    async function getTransactions
-
-    function deleteTransaction(id){
-        dispatch({
-            type:'DELETE_TRANSACTION',
-            payload:id
-        })
+    async function getTransactions(){
+        try {
+            const res = await axios.get('api/v1/transactions');
+            dispatch({
+                type:'GET_TRANSACTION',
+                payload:res.data.data
+            });
+        } catch (error) {
+            dispatch({
+                type:'TRANSACTION_ERROR',
+                payload:error.response.data.error
+            });
+        }
     }
-    function addTransaction(transaction){
+
+   async function deleteTransaction(id){
+        try {
+            await axios.delete(`/api/v1/transactions/${id}`);
+            dispatch({
+                type:'DELETE_TRANSACTION',
+                payload: id
+            });
+        } catch (error) {
+            dispatch({
+                type:'TRANSACTION_ERROR',
+                payload:error.response.data.error
+            });
+        }
+    }
+async function addTransaction(transaction){
+    const config = {
+        headers:{
+            'Content-Type':'application/json'
+        }
+    }
+    try {
+        const res  = await axios.post('/api/v1/transactions',transaction,config);
         dispatch({
             type:'ADD_TRANSACTION',
-            payload: transaction
-        })
+            payload: res.data.data
+        });
+    } catch (error) {
+        dispatch({
+            type:'TRANSACTION_ERROR',
+            payload:error.response.data.error
+        });
+    }
+       
     }
     return (<GlobalContext.Provider value={
         {transactions:state.transactions,
+            error:state.error,
+            loading:state.loading,
+            getTransactions,
             deleteTransaction,
             addTransaction
         }
