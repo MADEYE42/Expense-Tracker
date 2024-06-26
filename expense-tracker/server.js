7b0100a5b7e1e01f408cfc76277ee9f973cafeaa
Gouresh  Madye <117588160+MADEYE42@users.noreply.github.com>
2024-06-26 13:48:19 +0530

    All configs done

--- expense-tracker/server.js
+++ expense-tracker/server.js
@@ -1,8 +1,10 @@
+const path = require('path')
 const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
+require('dotenv').config(); 
 
 dotenv.config({path:'./config/config.env'});
 
@@ -20,6 +22,12 @@ if(process.env.NODE_ENV === 'development'){
 
 app.use('/api/v1/transactions',transactions);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
+
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port - ${PORT}`.yellow.bold));
+app.listen(PORT,()=> console.log(`Server running in ${process.env.NODE_ENV} mode on port - ${PORT}`.yellow.bold));
