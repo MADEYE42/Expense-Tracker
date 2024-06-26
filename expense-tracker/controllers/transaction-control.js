f43c9408fe19a7fb6d3a597c21072f2664547396
Gouresh  Madye <117588160+MADEYE42@users.noreply.github.com>
2024-06-26 11:22:21 +0530

    additional

--- expense-tracker/controllers/transaction-control.js
+++ expense-tracker/controllers/transaction-control.js
@@ -2,3 +2,9 @@
 exports.getTransactions = (req,res,next)=>{
     res.send('GET Transactions')
 }
+exports.addTransactions = (req,res,next)=>{
+    res.send('POST Transactions')
+}
+exports.deleteTransactions = (req,res,next)=>{
+    res.send('DELETE Transactions')
+}
--- expense-tracker/routes/transactions.js
+++ expense-tracker/routes/transactions.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router =  express.Router();
-const {getTransactions} = require('../controllers/transaction-control');
+const {getTransactions,addTransactions,deleteTransactions} = require('../controllers/transaction-control');
 
-router.route('/').get
+router.route('/').get(getTransactions).post(addTransactions);
+router.route("/:id").delete(deleteTransactions);
 module.exports = router;
